60b244d79bd3f0c66f7e639f4e5921a1e784d030
mataix.ab5 <mataix.ab@gmail.com>
2022-02-01 15:46:03 +0100

    Update email.js

--- jenkinsScripts/email.js
+++ jenkinsScripts/email.js
@@ -1,11 +1,7 @@
 const nodemailer = require('nodemailer');
 
 
-console.log(process.argv[3]);
 
-console.log(process.argv[4]);
-
-console.log(process.argv[5]);
 
 
 var transporter = nodemailer.createTransport({
